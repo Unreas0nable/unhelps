2a8e4506c17d8800c5da7ec4f27eb69b17d9de37
Unreas0nable <Unreas0nable@users.noreply.github.com>
2017-08-07 15:18:18 -0400

    Add files via upload

--- cmds/qhelp.js
+++ cmds/qhelp.js
@@ -2,19 +2,21 @@
 
 module.exports.run = async (bot, message, args) => {
         let embed = new Discord.RichEmbed()
-            .setTitle("Help Panel")
-            .setDescription("Use u? to execute a command shown below")
-            .setColor("#DB3498")
-            .addField("capri",
+        .setTitle("Help Panel")
+        .setDescription("Use u? to execute a command shown below")
+        .setColor("#DB3498")
+        .addField("capri",
 	    "---------")
 	    .addField("help",
-            "to show the normal commands")
+        "to show the normal commands")
 	    .addField("pokohknox",
 	    "---------")
-            .addField("saruze",
-            "---------")
-            .addField("End of Help Panel",
-            "There will be more quotes, message Unfσrgσττεn [Discord.js]#9982 if you want your quote to be requested.");
+        .addField("saruze",
+        "---------")
+        .addField("saruze2",
+        "added - 08/07/2017 3:20 PM EST")
+        .addField("End of Help Panel",
+        "There will be more quotes, message Unfσrgσττεn [Discord.js]#9982 if you want your quote to be requested.");
 
         message.channel.send({embed: embed});
 
