76d5cf0afc79cec60b15430b133630a750c1728d
Unreas0nable <Unreas0nable@users.noreply.github.com>
2017-08-07 17:31:13 -0400

    Update SakiBot.js

--- SakiBot.js
+++ SakiBot.js
@@ -84,6 +84,7 @@ bot.on('message', msg => {
         `$qhelp is for a quote help menu..\n` +
         `Saki Bot is still work in progress.. if you are a tester or just wants to become a tester, type $invite\n` +
         `If there's any bugs or glitch, contact Unfσrgσττεn死ね#9982 **- the owner of Saki Bot**\n` +
+	`Eval aka Calculator will be added..` +
         "\n```")
         }
     });
